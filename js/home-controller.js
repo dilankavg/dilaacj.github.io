fe6744438de8402d27bc7c3b57529ce05ac816af
dilankavg <31626252+dilankavg@users.noreply.github.com>
2017-09-11 18:37:27 +0530

    chessgame

---
+++ js/home-controller.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Dilanka Virajith on 9/5/2017.
+ */
+var LetterArray = ['A','B','C','D','E','F','G','H']
+var NumberArray = [1,2,3,4,5,6,7,8]
+
+var currentPosstion;
+var suares;
+var chesspieces;
+
+var pondCount;
+var CountArray =new Array(8);
+
+$(document).ready(function () {
+    suares=$('.square');
+    chesspieces=$('.pieces');
+})
